a8cae1e612b8d4af2f1964d1c818cfbe1b63af22
Abhi96515 <103572307+Abhi96515@users.noreply.github.com>
2023-08-18 02:55:53 +0530

    completed hero section

--- MOVIEVERSE/src/App.jsx
+++ MOVIEVERSE/src/App.jsx
@@ -19,13 +19,20 @@ function App() {
       state.home)
       console.log(url)
         useEffect(()=>{
-          apiTesting();
+          fetchApiConfig();
         },[])
 
-      const apiTesting=()=>{
-      fetchDataFromApi("/movie/popular").then((res)=> {
+      const fetchApiConfig=()=>{
+      fetchDataFromApi("/configuration").then((res)=> {
         console.log(res)
-        dispatch(getApiConfiguration(res))
+
+        const url ={
+          backdrop: res.images.secure_base_url + 'original',
+          poster: res.images.secure_base_url + 'original',
+          profile: res.images.secure_base_url + 'original'
+        }
+
+        dispatch(getApiConfiguration(url))
         })
   }
   return (
--- MOVIEVERSE/src/pages/home/heroBanner/HeroBanner.jsx
+++ MOVIEVERSE/src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,14 +1,24 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
 import "./style.scss"
+import { useSelector } from 'react-redux';
 import useFetch from '../../../hooks/useFetch'
-
+import Img from '../../../components/lazyLoadingImage/Img';
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
 
 const HeroBanner = () => {
   const [background,setbackground]=useState("")
   const [query,setquery]=useState("")
   const navigate= useNavigate()
   const {data,loading}=useFetch('/movie/upcoming')
+  const {url} = useSelector((state)=>state.home)
+
+  useEffect(()=>{
+     const bg= url.backdrop +
+     data?.results?.[Math.floor(Math.random()*20)]?.backdrop_path;
+     setbackground(bg)
+
+  },[data])
 
   const seachQuerySelector=(event)=>{
     if(event.key==='Enter'& query.length>0){
@@ -18,7 +28,14 @@ const HeroBanner = () => {
 
   return (
     <div className='heroBanner'>
-      <div className="wrapper">
+      {!loading &&(
+       <div className="backdrop-img">
+        <Img src={background}/>
+      </div>)}
+
+      <div className="oppacity-layer"></div>
+
+      <ContentWrapper>
         <div className="heroBannerContent">
           <span className="title">Welcome</span>
           <span className="subTitle">
@@ -32,8 +49,8 @@ const HeroBanner = () => {
             />
             <button>Search</button>
           </div>
-        </div>
-      </div>
+        </div> 
+      </ContentWrapper>
     </div>
   )
 }

6c1696a19b0d1257fa1f8176cf0feaf5db8e2f3b
Abhi96515 <103572307+Abhi96515@users.noreply.github.com>
2023-08-25 02:06:18 +0530

    add details page

--- MOVIEVERSE/src/pages/details/Detail.jsx
+++ MOVIEVERSE/src/pages/details/Detail.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import "./Detail.scss"
+import DetailsBanner from './detailsBanner/DetailsBanner'
 const Detail = () => {
   return (
-    <div>Detail</div>
+    <div>
+      <DetailsBanner/>
+    </div>
   )
 }
 
---
+++ MOVIEVERSE/src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import dayjs from "dayjs";
+
+import "./style.scss";
+
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import useFetch from "../../../hooks/useFetch";
+
+import Genres from "../../../components/genres/Genres";
+import CircleRating from "../../../components/cirleRating/CircleRating";
+import Img from "../../../components/lazyLoadingImage/Img";
+import PosterFallback from "../../../assets/Moviex-images/no-poster.png";
+
+const DetailsBanner = ({ video, crew }) => {
+
+    const toHoursAndMinutes = (totalMinutes) => {
+        const hours = Math.floor(totalMinutes / 60);
+        const minutes = totalMinutes % 60;
+        return `${hours}h${minutes > 0 ? ` ${minutes}m` : ""}`;
+    };
+
+    return (
+        <div className="detailsBanner">
+            {!loading ? (
+                <div>Details Content...</div>
+            ) : (
+                <div className="detailsBannerSkeleton">
+                    <ContentWrapper>
+                        <div className="left skeleton"></div>
+                        <div className="right">
+                            <div className="row skeleton"></div>
+                            <div className="row skeleton"></div>
+                            <div className="row skeleton"></div>
+                            <div className="row skeleton"></div>
+                            <div className="row skeleton"></div>
+                            <div className="row skeleton"></div>
+                            <div className="row skeleton"></div>
+                        </div>
+                    </ContentWrapper>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default DetailsBanner;

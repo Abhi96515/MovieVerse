c20297acb3b133377802d13042b7e4118471891f
Abhi96515 <103572307+Abhi96515@users.noreply.github.com>
2023-08-26 03:05:42 +0530

    created detail banner section

--- MOVIEVERSE/src/pages/details/Detail.jsx
+++ MOVIEVERSE/src/pages/details/Detail.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { createBrowserRouter, useParams } from 'react-router-dom'
 import "./Detail.scss"
 import DetailsBanner from './detailsBanner/DetailsBanner'
+import useFetch from '../../hooks/useFetch'
 const Detail = () => {
+  const {mediaType,id}=useParams()
+  const {data,loading}=useFetch(`/${mediaType}/${id}/videos`)
+  const {data: credits,loading:creditLoading}=useFetch(`/${mediaType}/${id}/credits`)
+
   return (
     <div>
-      <DetailsBanner/>
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew}/>
     </div>
   )
 }

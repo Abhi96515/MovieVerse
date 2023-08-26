7f6c67827c1538323f3d787939a613026cebf319
Abhi96515 <103572307+Abhi96515@users.noreply.github.com>
2023-08-27 03:00:29 +0530

    reacted some other components in detail pages

--- MOVIEVERSE/src/pages/details/Detail.jsx
+++ MOVIEVERSE/src/pages/details/Detail.jsx
@@ -3,6 +3,10 @@ import { createBrowserRouter, useParams } from 'react-router-dom'
 import "./Detail.scss"
 import DetailsBanner from './detailsBanner/DetailsBanner'
 import useFetch from '../../hooks/useFetch'
+import Cast from './cast/Cast'
+import VideosSection from './videosSection/VideosSection'
+import Similar from './carousels/Similar'
+import Recommendation from './carousels/Recommendation'
 const Detail = () => {
   const {mediaType,id}=useParams()
   const {data,loading}=useFetch(`/${mediaType}/${id}/videos`)
@@ -11,6 +15,10 @@ const Detail = () => {
   return (
     <div>
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew}/>
+      <Cast data={credits?.cast} loading={creditLoading}/>
+      <VideosSection data={data} loading={loading}/>
+      <Similar mediaType={mediaType} id={id}/>
+      <Recommendation mediaType={mediaType} id={id}/>
     </div>
   )
 }

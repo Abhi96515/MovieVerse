176f1ae260215ed97cf826a8f8f27e806a00999c
Abhi96515 <103572307+Abhi96515@users.noreply.github.com>
2023-08-30 01:10:12 +0530

    c

--- MOVIEVERSE/src/pages/404/PageNotFound.jsx
+++ MOVIEVERSE/src/pages/404/PageNotFound.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import './style.scss'
-const PageNotFound = () => {
-  return (
-    <div>PageNotFound</div>
-  )
-}
 
-export default PageNotFound 
+import React from "react";
+
+import "./style.scss";
+
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default PageNotFound;
